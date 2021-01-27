70eec145b7fb37d1c69fec5723949c4574d9a3a1
Kacper Maj <60718448+kapisolec@users.noreply.github.com>
2021-01-27 12:53:18 +0100

    Synchornization on join + chat bugfixes

---
+++ src/utils/videos.js
@@ -0,0 +1,26 @@
+const videos = [];
+
+const addVideo = ({ room, videoID }) => {
+    const videoIsTheSame = videos.find((obj) => obj.videoID === videoID);
+    if (videoIsTheSame) return;
+    const index = videos.findIndex((object) => object.room === room);
+    if (index === -1) {
+        videos.push({ room, videoID });
+        console.log(videos);
+        return videos;
+    }
+    videos.splice(index, 1, { room, videoID });
+    console.log(videos);
+    return videos;
+};
+
+const getVideo = (room) => {
+    const video = videos.find((object) => object.room === room);
+    if (!video) return;
+    return video.videoID;
+};
+
+module.exports = {
+    addVideo,
+    getVideo,
+};
